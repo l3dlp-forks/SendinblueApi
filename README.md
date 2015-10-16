a4ec6c34b92315eb7953229e37d602ac0a83a779
Joni <joni@sendinblue.com>
2015-10-16 16:45:11 +0530

    Email api implemented

--- README.md
+++ README.md
@@ -57,6 +57,32 @@ $sendinblue = new Sendinblue();
 $sendinblue->getSmsApi()->sendSms('+33600000000', 'Your name', 'The message you want to send');
 ```
 
+Email Api usage
+-------------
+
+<?php
+
+require 'vendor/autoload.php';
+
+use Scoringline\SendinblueApi\Sendinblue;
+
+$sendinblue = new Sendinblue();
+
+$sendinblue->getEmailApi()->sendEmail(
+    ['to@example.com'=> 'to name!'],
+    ['from@yahoo.com', 'from email or name!'],
+    'Invitation',
+    'You are invited for giving test',
+    'This is the <h1>HTML</h1>'
+    ['replyto@yahoo.com', 'reply to!'],
+    ['cc@example.com' => 'cc name'],
+    ['bcc@example.com' => 'bcc name'],
+    [],
+    ['Content-Type" => "text/html; charset=iso-8859-1'],
+    ['myinlineimage1.png' => 'your_png_files_base64_encoded_chunk_data']
+); 
+
+
 ----------------------------------------------------------------
 
 This library is provided to you by [Scoringline](http://en.scoringline.com), if you're searching for more efficient hiring, checkout our application !
---
+++ src/Scoringline/SendinblueApi/Api/Email.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is a part of scoringline sendinblue api package
+ *
+ * (c) Scoringline <joni@sendinblue.com>
+ *
+ * For the full license, take a look to the LICENSE file
+ * on the root directory of this project
+ */
+namespace Scoringline\SendinblueApi\Api;
+
+use Nekland\BaseApi\Api\AbstractApi;
+
+class Email extends AbstractApi
+{
+    const API_URL = '/email';
+    /**
+     * @param array  $to         I.e: ['to@example.com'=> 'to name'] associative array with commas to separate multiple recipients
+     * @param array  $from       I.e: ['from@yahoo.com', 'from email']
+     * @param string $subject    I.e: "Invitation"
+     * @param string $text       I.e: "You are invited for giving test!"
+     * @param string $html       I.e: "This is the <h1>HTML</h1>"
+     * @param array  $replyTo    I.e: ['replyto@yahoo.com', 'reply to']
+     * @param array  $cc         I.e: ['cc@example.com' => 'cc name']
+     * @param array  $bcc        I.e: ['bcc@example.com' => 'bcc name']
+     * @param array $attachment
+     * @param array $headers     I.e ["Content-Type" => "text/html; charset=iso-8859-1"]
+     * @param array $inlineImage I.e ['YourFileName.Extension' => 'Base64EncodedChunkData'). associative array
+     * @return array
+     * @throws \RuntimeException
+     */
+    public function sendEmail(
+        $to,
+        $from,
+        $subject,
+        $text,
+        $html,
+        $replyTo = [],
+        $cc = [],
+        $bcc = [],
+        $attachment = [],
+        $headers = [],
+        $inlineImage = []
+    ) {
+        $result = $this->post(self::API_URL, json_encode([
+            'to' => $to,
+            'from'=> $from,
+            'subject' => $subject,
+            'text' => $text,
+            'html' => $html,
+            'replyto' => $replyTo,
+            'cc' => $cc,
+            'bcc' => $bcc,
+            'attachment' => $attachment,
+            'headers'=> $headers,
+            'inline_image' => $inlineImage
+        ]));
+
+        if ($result['code'] === 'failure') {
+            throw new \RuntimeException($result['message']);
+        }
+
+        return $result;
+    }
+}
